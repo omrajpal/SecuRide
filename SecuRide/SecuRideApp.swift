aa8b33e407f056c2101a2fb2c42c61a156cba7f3
omrajpal <omrajpal15@gmail.com>
2024-07-14 12:18:55 -0500

    Initial Commit

---
+++ SecuRide/SecuRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecuRideApp.swift
+//  SecuRide
+//
+//  Created by Om Rajpal on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SecuRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
