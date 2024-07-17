9a2968f1cc3414554b41061b18a08ccb2bf024fd
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-07-17 18:01:07 -0500

    carthage

--- Pods/Target Support Files/Pods-SecuRide-SecuRideUITests/Pods-SecuRide-SecuRideUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SecuRide_SecuRideUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SecuRide_SecuRideUITests
-@end
--- Pods/Target Support Files/UberCore/UberCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UberCore : NSObject
-@end
-@implementation PodsDummy_UberCore
-@end
