2ef155d1fa00309b2225a3c4d599f05e2ddaf52f
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-08-16 16:33:35 -0500

    maps

---
+++ SecuRide/Screens/HomeMapView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeMapView.swift
+//  SecuRide
+//
+//  Created by Ashok Saravanan on 8/16/24.
+//
+
+import SwiftUI
+
+struct HomeMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeMapView()
+}
